b2222b0bda7c4da81954ecbd6a8e83b17a5f8649
Marc Christenfeldt <marc.christenfeldt@gmx.de>
2025-05-05 03:42:15 +0200

    chore: Add debug script for inspector - Creates a new debug.sh script to easily start the inspector - Configures database connection parameters - Sets up MARIADB environment variables for development

---
+++ debug.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#
+# starts inspector
+#
+
+MARIADB_HOST="localhost" MARIADB_PORT="10136" MARIADB_USER="root" MARIADB_PASSWORD="11111" MARIADB_DATABASE="t2" MARIADB_ALLOW_INSERT="true" MARIADB_ALLOW_UPDATE="true" MARIADB_ALLOW_DELETE="true" MARIADB_TIMEOUT_MS="10000" MARIADB_ROW_LIMIT="1000" npx @modelcontextprotocol/inspector@latest ./dist/index.js
